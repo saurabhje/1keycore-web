40b829320fecf5956c5730b770e0f7230e046819
saurabhje <jeiolsatszd@gmail.com>
2026-04-17 18:43:57 +0000

    Add: readme

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,6 @@ export default function Home() {
         <Hero/>
         <HowItWorks />
         <Features />
-        {/* <ProvidersStats />
-        <CTASection /> */}
       </main>
       <Footer />
     </>
---
+++ readme.md
@@ -0,0 +1,4 @@
+# 1keycore - the intilligent llm gateway
+This repo contains the UI code for (1keycore)[github.com/saurabhje/1keycore]
+![Block Level Architecture](image.png)
+
